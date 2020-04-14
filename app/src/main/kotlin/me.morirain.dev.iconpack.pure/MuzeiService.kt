d590f864726cad74431f4f5e878f32000b392b72
morirain <morirain@outlook.com>
2020-04-15 01:10:11 +0800

    update appfilter.xml

--- app/src/main/kotlin/me.morirain.dev.iconpack.pure/MuzeiService.kt
+++ app/src/main/kotlin/me.morirain.dev.iconpack.pure/MuzeiService.kt
@@ -15,7 +15,6 @@
  */
 package me.morirain.dev.iconpack.pure
 
-import jahirfiquitiva.libs.frames.data.services.FramesArtSource
+import jahirfiquitiva.libs.frames.data.services.FramesArtProvider
 
-// TODO: Use a unique name instead of MyBlueprintMuzeiService
-class MuzeiService : FramesArtSource("PureIconPackMuzeiService")
+class MuzeiService : FramesArtProvider()
