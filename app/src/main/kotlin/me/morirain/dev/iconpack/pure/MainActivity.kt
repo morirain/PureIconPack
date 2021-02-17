43406e325ed81e6d2dda035d10f3977608058da5
morirain <morirain@outlook.com>
2021-02-17 13:12:15 +0800

    Custom "SendIconRequest"

--- app/src/main/kotlin/me/morirain/dev/iconpack/pure/MainActivity.kt
+++ app/src/main/kotlin/me/morirain/dev/iconpack/pure/MainActivity.kt
@@ -2,9 +2,18 @@ package me.morirain.dev.iconpack.pure
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import com.github.javiersantos.piracychecker.PiracyChecker
+import com.google.gson.GsonBuilder
+import dev.jahir.blueprint.data.requests.ArcticService
+import dev.jahir.blueprint.data.requests.SendIconRequest
 import dev.jahir.blueprint.ui.activities.BottomNavigationBlueprintActivity
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.*
+import kotlin.math.log
+import kotlin.reflect.jvm.javaField
 
 /**
  * You can choose between:
@@ -12,6 +21,25 @@ import java.util.*
  * - BottomNavigationBlueprintActivity
  */
 class MainActivity : BottomNavigationBlueprintActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (BuildConfig.DEBUG) {
+            val request = lazy {
+                Retrofit.Builder()
+                    .baseUrl("https://morirain-3gv3co56b4babf1b-1256096275.ap-shanghai.app.tcloudbase.com/IconReceive/")
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                    .build().create(ArcticService::class.java)
+            }
+            val c = SendIconRequest::class.java.getDeclaredField("INSTANCE").get(null)
+            c.javaClass.getDeclaredField("service\$delegate").let { field ->
+                field.isAccessible = true
+                field.set(SendIconRequest, request)
+                return@let field.get(SendIconRequest)
+            }
+        }
+        super.onCreate(savedInstanceState)
+    }
 
     /**
      * These things here have the default values. You can delete the ones you don't want to change
@@ -28,7 +56,8 @@ class MainActivity : BottomNavigationBlueprintActivity() {
      * This is your app's license key. Get yours on Google Play Dev Console.
      * Default one isn't valid and could cause issues in your app.
      */
-    override fun getLicKey(): String? = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAourVcAxhBy+MOy+zDZM01ntfDrJ5A3cHZqoEcJiwz2jRrmfByDBRPjKUCkJoEACknJU2WxyPFesI9ncLucL+nrcWoNZwK/132ltpOngE4Muqi9z4GVmFLqcj8I7ABCg08eyHf3pTrbOX8l+w1EynoLGZ1oPIRHc3mQq04bGyQL43C5R1/Holq4A1tYScvy/5E2HFf9bGFOc9YOVSdgmOJ27gfXptDGtfaznJLUqK91vCsx4TO8fvlLXwZtlT8w50hyK0XO04NyFEae+Z9qxuL4Zvp5FVkyfuB1uPggWvBtoiUxjtYNVfj8TxUu7k7zHqkR7Q9EHys7p/GqjQCr9FVwIDAQAB"
+    override fun getLicKey(): String? =
+        "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAourVcAxhBy+MOy+zDZM01ntfDrJ5A3cHZqoEcJiwz2jRrmfByDBRPjKUCkJoEACknJU2WxyPFesI9ncLucL+nrcWoNZwK/132ltpOngE4Muqi9z4GVmFLqcj8I7ABCg08eyHf3pTrbOX8l+w1EynoLGZ1oPIRHc3mQq04bGyQL43C5R1/Holq4A1tYScvy/5E2HFf9bGFOc9YOVSdgmOJ27gfXptDGtfaznJLUqK91vCsx4TO8fvlLXwZtlT8w50hyK0XO04NyFEae+Z9qxuL4Zvp5FVkyfuB1uPggWvBtoiUxjtYNVfj8TxUu7k7zHqkR7Q9EHys7p/GqjQCr9FVwIDAQAB"
 
     /**
      * This is the license checker code. Feel free to create your own implementation or
@@ -38,7 +67,7 @@ class MainActivity : BottomNavigationBlueprintActivity() {
      */
     override fun getLicenseChecker(): PiracyChecker? {
         destroyChecker() // Important
-        val l:Locale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        val l: Locale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
             MyApplication.appContext.resources.configuration.locales.get(0)
         else
             MyApplication.appContext.resources.configuration.locale
@@ -47,7 +76,8 @@ class MainActivity : BottomNavigationBlueprintActivity() {
         return if (BuildConfig.DEBUG)
             return null
         else
-            super.getLicenseChecker()
+            return null
+        //super.getLicenseChecker()
     }
 
     override fun defaultTheme(): Int = R.style.MyApp_Default
--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "me.morirain.dev.iconpack.pure"
-    const val version = 1920110101
-    const val versionName = "8.0"
+    const val version = 2020021700
+    const val versionName = "8.2"
 }
