ef6d7e3cdce4786451e8259c24e1763ef1f57dbf
morirain <morirain@outlook.com>
2021-05-31 02:28:47 +0800

    8.5

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "me.morirain.dev.iconpack.pure"
-    const val version = 2020042801
-    const val versionName = "8.4"
+    const val version = 2021053101
+    const val versionName = "8.5"
 }
