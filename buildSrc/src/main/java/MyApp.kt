56a2124a7985a0fdaadd7f150cc9345d67a24257
morirain <morirain@outlook.com>
2023-08-08 22:25:07 +0800

    2023 Updated

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "me.morirain.dev.iconpack.pure"
-    const val version = 2022072501
-    const val versionName = "20220725"
+    const val version = 2023080301
+    const val versionName = "20230803"
 }
