11decb3c4d74a4475737daf085b8c7145d941635
morirain <morirain@outlook.com>
2026-07-05 03:21:31 +0800

    20260704

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "me.morirain.dev.iconpack.pure"
-    const val version = 2025072303
-    const val versionName = "20250723"
+    const val version = 2026070401
+    const val versionName = "20260704"
 }
